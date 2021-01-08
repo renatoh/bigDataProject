7051f307ee59e961dc1058e1fb7e8323468e6a95
Thomas Vinzenz <60885756+vinzenzt@users.noreply.github.com>
2021-01-08 13:41:05 +0100

    Update LogAnalyzerPyspark.py
    add function calc_error which calculates the error of a given point to its cluster center and store the result to the disk (as RDD) if a certain threshold is exceeded

--- pythonsrc/LogAnalyzerPyspark.py
+++ pythonsrc/LogAnalyzerPyspark.py
@@ -9,14 +9,16 @@ Created on Mon Jan  4 11:54:19 2021
 import re
 from math import sqrt
 from numpy import array
+from datetime import datetime
 
 from pyspark import SparkContext, SparkConf
 from pyspark.streaming import StreamingContext
 from pyspark.sql import Row
 from pyspark.sql import SQLContext
-from pyspark.ml.feature import VectorAssembler
-from pyspark.ml.clustering import KMeans
+from pyspark.ml.feature import VectorAssembler, StringIndexer, OneHotEncoder, Normalizer
+from pyspark.ml.clustering import KMeans, KMeansModel
 from pyspark.ml.evaluation import ClusteringEvaluator
+from pyspark.ml import Pipeline
 
 from pythonsrc.LogFileParser import parse_apache_log_line
 
@@ -25,21 +27,44 @@ sc = SparkContext(conf=conf)
 ssc = StreamingContext(sc, 10)
 sqlContext = SQLContext(sc)
 
+RDD_LOCATION = '../resources/'
+
 SOCKET_HOST = '84.20.60.172'
 SOCKET_PORT = 8080
 
 def error(point, cluster_center):
     return sqrt(sum([x**2 for x in (point - cluster_center)]))
 
+def calc_error(rdd):
+    now = datetime.now()
+    data = trainingData.toDF()
+    
+    indexers = [ StringIndexer(inputCol=c, 
+                           outputCol="{0}_indexed".format(c)) for c in ['endpoint','method'] ]
+    encoders = [ OneHotEncoder(inputCol=indexer.getOutputCol(),
+                 outputCol="{0}_encoded".format(indexer.getOutputCol()))
+                 for indexer in indexers ]
+    assembler = VectorAssembler(inputCols=['response_code', 'content_size'] + [encoder.getOutputCol() for encoder in encoders], 
+                                outputCol='features')
+    pipeline = Pipeline(stages=indexers + encoders + [assembler])
+    modelTransform=pipeline.fit(data)
+    output = modelTransform.transform(data)
+    predictions = model.transform(output)
+  
+    wssse = predictions.select(['features','prediction'])\
+      .rdd\
+      .map(lambda line: error(line[0],clusterCenters[line[1]]))\
+      .filter(lambda x: x > 125100.0)
+    if wssse.count() > 0:
+        wssse.saveAsTextFile(RDD_LOCATION+str(now))
+    return wssse
+
 access_logs = ssc.socketTextStream(SOCKET_HOST, SOCKET_PORT)
-#access_logs = ssc.textFileStream('/Users/thomas/Downloads/logstream/')
-struc_logs = access_logs.map(parse_apache_log_line)
+struc_logs = access_logs.map(lambda line: parse_apache_log_line(line, re))
 struc_logs.pprint()
 rc_dstream = struc_logs.map(lambda parsed_line: (parsed_line.response_code, 1)) 
 rc_count = rc_dstream.reduceByKey(lambda x,y: x+y)
 rc_count.pprint(num = 30)
-cluster_dstream = struc_logs.map(lambda parsed_line: (parsed_line.response_code, parsed_line.content_size)) 
-#result = cluster_dstream.map(lambda point: error(point))
-#result.pprint()
+struc_logs.foreachRDD(calc_error)
 ssc.start()
 ssc.awaitTermination()
