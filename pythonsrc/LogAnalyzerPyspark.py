1bf109dc9107171a9373883aad3a5f98dd9070ef
Thomas Vinzenz <60885756+vinzenzt@users.noreply.github.com>
2021-01-08 13:32:20 +0100

    Update LogAnalyzerPyspark.py
    remove model part and add LogFileParser as lib

--- pythonsrc/LogAnalyzerPyspark.py
+++ pythonsrc/LogAnalyzerPyspark.py
@@ -18,63 +18,18 @@ from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.clustering import KMeans
 from pyspark.ml.evaluation import ClusteringEvaluator
 
+from pythonsrc.LogFileParser import parse_apache_log_line
+
 conf = SparkConf().setAppName("Log Analyzer")
 sc = SparkContext(conf=conf)
 ssc = StreamingContext(sc, 10)
 sqlContext = SQLContext(sc)
 
-APACHE_ACCESS_LOG_PATTERN = '^(\S+) (\S+) (\S+) \[([\w:\/]+\s)\] "(\S+) (\S+) (\S+)" (\d{3}) (\d+)'
-
 SOCKET_HOST = '84.20.60.172'
 SOCKET_PORT = 8080
-FILE_TRAINING_DATA = '/Users/thomas/Downloads/logstream/apache2.log'
-
-def parse_apache_log_line(logline):
-    match = re.search(APACHE_ACCESS_LOG_PATTERN, logline)
-    if match is None:
-        raise Exception("Invalid logline: %s" % logline)
-    return Row(
-        ip_address    = match.group(1),
-        client_identd = match.group(2),
-        user_id       = match.group(3),
-        date = (match.group(4)[:-6]).split(":", 1)[0],
-        time = (match.group(4)[:-6]).split(":", 1)[1],
-        method        = match.group(5),
-        endpoint      = match.group(6),
-        protocol      = match.group(7),
-        response_code = int(match.group(8)),
-        content_size  = int(match.group(9))
-    )
 
 def error(point, cluster_center):
     return sqrt(sum([x**2 for x in (point - cluster_center)]))
-
-#train modell (based on REsponde Code and Content Size for simplicity)
-trainingData = sc.textFile('/Users/thomas/Downloads/logstream/apache2.log')\
-  .map(parse_apache_log_line)
-data = trainingData.toDF()
-assembler = VectorAssembler(inputCols=['response_code', 'content_size'], outputCol='features')
-output = assembler.transform(data)
-kmeans = KMeans().setK(2).setSeed(1)
-model = kmeans.fit(output)
-clusterCenters = model.clusterCenters()
-
-predictions.printSchema()
-
-#check model performacne by means of silhouette coefficient
-predictions = model.transform(output)
-evaluator = ClusteringEvaluator()
-silhouette = evaluator.evaluate(predictions)
-print(silhouette)
-wssse = predictions.select(['response_code','content_size','prediction'])\
-  .rdd.map(lambda line: (array([float(x) for x in line[0:1]]), clusterCenters[line[2]]))\
-  .map(lambda line: error(line[0],line[1]))\
-  .reduce(lambda x, y: x + y)
-print(wssse)
-
-#def error(point):
-#    center = clusters.centers[clusters.predict(point)]
-#    return sqrt(sum([x**2 for x in (point - center)]))
 
 access_logs = ssc.socketTextStream(SOCKET_HOST, SOCKET_PORT)
 #access_logs = ssc.textFileStream('/Users/thomas/Downloads/logstream/')
